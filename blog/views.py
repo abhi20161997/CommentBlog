ceb3e1855ce657822ed0893e673a23cfb6d9bab4
Abhijit Kumar <f2016826@pilani.bits-pilani.ac.in>
2017-09-29 15:52:37 +0530

    Multiline comment functionality added- Project complete

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from .models import Post
-
+from .models import Post,Comment
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404,redirect
 
 def post_list(request):
 	posts=Post.objects.all()
@@ -14,3 +16,32 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
